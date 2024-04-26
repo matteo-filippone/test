5d74e80306de226273b4b86caec5fa6f49a94e6a
Lara Filippone <lara.filippone@gmail.com>
2024-04-26 19:59:32 +0200

    esercizio per 27 aprile

---
+++ esercizio_sabato_27_aprile.py
@@ -0,0 +1,4 @@
+# Scrivi una funzione in Python che accetti tre numeri come parametri e restituisca il
+# numero più grande tra i tre (non utilizzare la funzione max())
+# pensa a un nome comunicativo
+
